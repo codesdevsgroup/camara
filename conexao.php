6f2b8398b55012e5ca9dbca181bac04bd29d7cf9
Lucas Campos <bhlucascampos@gmail.com>
2022-04-04 01:00:44 -0300

    inicio

---
+++ conexao.php
@@ -0,0 +1,12 @@
+﻿<?php
+$servername="185.211.7.205";
+$database = "u127378936_camara";
+$username = "u127378936_codesdev";
+$password = "tQ7a=];H*4";
+// Create connection
+$mysqli = new mysqli($servername, $username, $password, $database);
+// Check connection
+if (!$msqli->connect_errno) {
+    echo "Falha na conexão: (".$mysqli->connect_error.") ".$mysqli->connect_error;
+}
+?>

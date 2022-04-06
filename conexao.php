89c42b9fceadceb4dcdd02a85071341896651139
Lucas Campos <bhlucascampos@gmail.com>
2022-04-06 20:03:24 -0300

    ignore conecta

--- conexao.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-    date_default_timezone_set('America/Sao_Paulo');
-	header('Content-Type: text/html; charset=UTF-8 Unicode (utf8)');
-	$dsn = "mysql:dbname=u127378936_camara;host:mysql.hostinger.com.br;port:3306";
-	    $user = "u127378936_codesdev";
-	    $senha = "tQ7a=];H*4"; 
-	$con = new PDO($dsn,$user, $senha);
-?>

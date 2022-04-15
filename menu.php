c2ee965b2c410c0dc01da35a358478cec8658849
Lucas Campos <bhlucascampos@gmail.com>
2022-04-15 14:51:11 -0300

    base

--- menu.php
+++ menu.php
@@ -18,15 +18,17 @@
   </div>
     <div id="menu">
         <div class="logo"><img src="img/logo.png" alt="Logo" class="logo" ></div>
+        <!-- navegação do site -->
         <nav>
-            <ul>
+            <div class="navega"><ul>
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="cidade.html">A Cidade</a></li>
                 <li><a href="camara.html">A Câmara</a></li>
                 <li><a href="legislacao.html">Legislação</a></li>
                 <li><a href="midia.html">Mídia</a></li>
-            </ul>
+            </ul></div>
         </nav>
+
     </div>
 </body>
 </html>

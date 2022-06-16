3ed3ffd3a81183a504be1f6e9ac4ae103d2f92a2
Lucas Campos <bhlucascampos@gmail.com>
2022-06-16 14:37:04 -0300

    ajuste de nomes de arquivos

--- header.php
+++ header.php
@@ -7,9 +7,19 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <title>Câmara Municipal de Materlândia - MG</title>
+
+  <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+   <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/style-starter.css">   
 </head>
 <body>
+  <div>
+    <nav>
+    
 
+    </nav>
+  </div>
     <!-- REMOVER NO FINAL -->
 
   <!-- Optional JavaScript --> <!-- jQuery first, then Popper.js, then Bootstrap JS -->

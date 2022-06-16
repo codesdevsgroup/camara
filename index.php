213f262f8e59344d7ff0aecb49f58c0099cacbbf
Lucas Campos <bhlucascampos@gmail.com>
2022-06-16 15:08:10 -0300

    criação da estrutura do site

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
--->
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Câmara Municipal de Materlândia - MG</title>
-    <!-- web fonts -->
-   <!-- web fonts -->
-   <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-   <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
-   <!-- //web fonts -->
-    <!-- //web fonts -->
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style-starter.css">
-  </head>
-  <body>
-  <?php
-			require_once('./conexao.php');
-			require_once('./function.php');
-	  cab();
-		 ?>
-
+<?php include 'header.php'; ?>
 <!-- Top Menu 1 -->
 
 <section class="w3l-main-slider" id="home">
@@ -330,9 +304,4 @@ Author URL: http://w3layouts.com
     })
   })
 </script>
-<!-- //script -->
-
-</body>
-
-</html>
-<!-- // grids block 5 -->
+<?php include 'footer.php'; ?>

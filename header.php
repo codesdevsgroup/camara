91d3339b05385f003fac65b09f78fb693a340e84
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-06-16 15:28:12 -0300

    header separado

--- header.php
+++ header.php
@@ -1,24 +1,25 @@
-<!doctype html>
-<html lang="en">
-  <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Câmara Municipal de Materlândia - MG</title>
-  <!-- web fonts -->
-  <!-- web fonts -->
-  <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-  <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
-  <!-- //web fonts -->
-  <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style-starter.css">
-  </head>
-  <body>
-  <?php
+<?php
 			require_once('./conexao.php');
 			require_once('./function.php');
 	  cab();
 		 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Câmara Municipal de Materlândia - MG</title>
+    <!-- web fonts -->
+   <!-- web fonts -->
+   <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+   <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
+   <!-- //web fonts -->
+    <!-- //web fonts -->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+  </head>
 
 
 <?php include 'footer.php'; ?>
--- index.php
+++ index.php
@@ -2,29 +2,11 @@
 Author: W3layouts
 Author URL: http://w3layouts.com
 -->
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Câmara Municipal de Materlândia - MG</title>
-    <!-- web fonts -->
-   <!-- web fonts -->
-   <link href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-   <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
-   <!-- //web fonts -->
-    <!-- //web fonts -->
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style-starter.css">
-  </head>
-  <body>
-  <?php
-			require_once('./conexao.php');
-			require_once('./function.php');
-	  cab();
+<?php
+			require_once('./header.php');
 		 ?>
+
+<body>
 
 <!-- Top Menu 1 -->
 

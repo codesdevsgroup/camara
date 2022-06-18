5af8acda133ee3766b939f98efc521449a75682d
Lucas Campos <bhlucascampos@gmail.com>
2022-06-18 17:36:03 -0300

    footer2

--- login.php
+++ login.php
@@ -4,9 +4,8 @@
   <head>
     <meta charset="utf-8" />
     <title>Login</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <style>
       .card-login {
         padding: 30px 0 0 0;
@@ -67,5 +66,4 @@
           </div>
         </div>
     </div>
-  </body>
-</html>
+<?php require_once 'footer2.php'; ?>

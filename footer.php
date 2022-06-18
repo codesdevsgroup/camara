80c238a483f8490acf503212c95e7c54c1ffb843
Lucas Campos <bhlucascampos@gmail.com>
2022-06-18 13:47:34 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -57,35 +57,6 @@
           </div>
       </div>
   </div>
-  <!-- move top -->
-  <button onclick="topFunction()" id="movetop" title="Go to top">
-    <span class="fa fa-angle-up"></span>
-  </button>
-  <script>
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction()
-    };
-
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("movetop").style.display = "block";
-      } else {
-        document.getElementById("movetop").style.display = "none";
-      }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
-  </script>
-  <!-- /move top -->
-</section>
-<script src="assets/js/jquery-3.3.1.min.js"></script>
-<!-- //footer-28 block -->
-</section>
 <script>
   $(function () {
     $('.navbar-toggler').click(function () {

8da6adbb8decd32b2bc65d2e67343a76dc4b54b2
Lucas Campos <bhlucascampos@gmail.com>
2022-06-17 23:31:02 -0300

    Update header.php

--- header.php
+++ header.php
@@ -10,13 +10,13 @@
 <body>
 <?php require_once('./conexao.php'); ?>
 <header>
-  <nav class="navbar navbar-expand-sm navbar-light bg-primary" style="border: 100px">
-    <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" width="110px"></a>
+  <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-primary" style="border: 100px">
+    <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" width="80px"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-target" >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="nav-target" >
-        <ul class="h5 ml-auto navbar-nav">
+        <ul class="h4 ml-auto navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="index.php">Início</a>
           </li>

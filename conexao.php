1d959eb6069721b6e27ef35c54e9d3c441520596
Lucas Campos <bhlucascampos@gmail.com>
2022-06-21 21:53:06 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -4,7 +4,6 @@ define('USER', 'root');
 define('PASS', '');
 define('DBNAME', 'camara');
 $conn = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . ';', USER, PASS);
-
 $mysqli = new mysqli(HOST, USER, PASS, DBNAME);
 if($mysqli->error) {
   die("Falha ao conectar ao banco de dados: " . $mysqli->error);

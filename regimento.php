b3d992ec6dcc4acc7c4363ca38e68f2d48191474
Lucas Campos <bhlucascampos@gmail.com>
2022-06-22 00:00:19 -0300

    Update regimento.php

--- regimento.php
+++ regimento.php
@@ -1,7 +1,29 @@
 <?php
 	require_once('./header.php');
 ?>
+<!-- content-with-photo4 block -->
 
+<!--/team-sec-->
+	<section class="w3l-team-main">
+		<div class="team py-5">
+			<div class="container py-lg-5">
+				<div class="heading text-center mx-auto">
+					<h3 class="head">Regimento Interno</h3>
+					<p class="my-3 head"> Aqui você encontra o Regimento Interno da Câmara Municipal de Materlândia - MG.</p>
+					  
+					</div>
+					
+					
+					
+					<div class="row team-row pt-3 mt-5">
+						
+						<iframe src="dados/producao/regimentointerno.pdf" width="95%" height="780" style="border: none;"></iframe>
+					
+							<!-- end team member -->
+				</div>
+			</div>
+	</section>
+	<!--//team-sec-->
 <!-- grids block 5 -->
 <section class="w3l-footer-29-main">
   <div class="footer-29">

095beb40c9c0ac558f22d2b521c9979ae4cd2551
Lucas Campos <bhlucascampos@gmail.com>
2022-06-30 00:33:00 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
 <section class="" style="clear: both;">
-  <div class="footer-29">
-      <div class="container">
+  <div class="container">
+      <div class="row">
+            <div class="footer-list-29 footer-3">     
+                <h6 class="footer-title-29">Câmara no Mapa </h6>  
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+            </div>
+            <div class="footer-list-29 footer-4">
+                <ul>
+                    <h6 class="footer-title-29">Links Úteis</h6>
+                    <li><a href="index.html">Pref. de Materlândia</a></li>
+                    <li><a href="about.html">Minas Gerais</a></li>
+                    <li><a href="services.html">Copasa</a></li>
+                    <li><a href="#"> Cemig</a></li>
+                    <li><a href="contact.html">Polícia Militar</a></li>
+                    <li><a href="https://webmail.hostinger.com.br/auth">Servidor de E-mail</a> </li>
+                </ul>
+              </div>
+          </div>
           <div class="d-grid grid-col-4 footer-top-29">
               <div class="footer-list-29 footer-1">
                   <h6 class="footer-title-29">Entre em Contato</h6>
@@ -27,26 +44,6 @@
                       <li><a href="contato.php.html">Contato</a></li>
                   </ul>
               </div>
-              <div class="footer-list-29 footer-3">
-                 
-                  <h6 class="footer-title-29">Câmara no Mapa </h6>
-                  
-                  
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        
-              </div>
-              <div class="footer-list-29 footer-4">
-                  <ul>
-                      <h6 class="footer-title-29">Links Úteis</h6>
-                      <li><a href="index.html">Pref. de Materlândia</a></li>
-                      <li><a href="about.html">Minas Gerais</a></li>
-                      <li><a href="services.html">Copasa</a></li>
-                      <li><a href="#"> Cemig</a></li>
-                      <li><a href="contact.html">Polícia Militar</a></li>
-                      <li><a href="https://webmail.hostinger.com.br/auth">Servidor de E-mail</a> </li>
-                  </ul>
-              </div>
-          </div>
           <div class="d-grid grid-col-2 bottom-copies">
               <p class="copy-footer-29">© 2022 Câmara de Materlândia. Todos os Direitos Reservados | <br>Desenvolvido por <a href="https://codesdevs.com">Codes Devs</a></p>
                <ul class="list-btm-29">
@@ -64,4 +61,3 @@
 
 </body>
 </html>
-<!-- // grids block 5 -->

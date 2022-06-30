7490a8106a52cea541ceec93569d0ae9d22a52c5
Lucas Campos <bhlucascampos@gmail.com>
2022-06-30 01:17:07 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
-<section class="">
+<section class="shadow pt-4 bg-light">
     <div class="container">
-        <div class="row">
-            <div class="footer-list-29 footer-3">     
-                <h6 class="footer-title-29">Câmara no Mapa </h6>  
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+        <div class="row ">
+            <div class="ml-2 col-md-5 text-md-start mb-md-0">     
+                <h6 class="">Câmara no Mapa </h6>  
+                <iframe class="ml-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" aria-hidden="false">
                 </iframe>
             </div>
             
-            <div>
+            <div class="col-md-6 text-md-end">
                 <ul>
                     <h6 class="">Entre em Contato</h6>
                     <li><p><span class=""></span><a href="https://goo.gl/maps/m2HFq5F2Vnd4jgLu9">Pça. Geraldo Anadir, 11, Centro, Materlândia - MG 39755-000.</a></p></li>
                     <li><a href="tel:+55 33 3427-1170"><span class="fa fa-phone"></span> +55 33 3427-1170</a></li>
-                    <li><a href="mailto:contato@materlandia.mg.leg.br" class="mail"><span class="fa fa-envelope-open-o"></span> contato@materlandia.mg.leg.br</a></li>
+                    <li><a href="mailto:contato@materlandia.mg.leg.br" class="mail"><span class=""></span> contato@materlandia.mg.leg.br</a></li>
                 </ul>
             </div>
         </div>
     
         <div class="row pt-3">
-            <div class="">
+            <div class="col-md-4 text-md-start mb-md-0">
                 <ul>
                     <h6 class="footer-title-29">Menu Principal</h6>
                     <li><a href="index.php">Início</a></li>
@@ -27,29 +27,33 @@
                     <li><a href="contato.php.html">Contato</a></li>
                 </ul>
             </div>
-            <div class="">
+            <div class="col-md-3 text-md-end">
                 <ul>
                     <h6 class="">Links Úteis</h6>
-                    <li><a href="index.html">Pref. de Materlândia</a></li>
-                    <li><a href="about.html">Minas Gerais</a></li>
-                    <li><a href="services.html">Copasa</a></li>
-                    <li><a href="#">Cemig</a></li>
-                    <li><a href="contact.html">Polícia Militar</a></li>
-                    <li><a href="https://webmail.hostinger.com.br/auth">Servidor de E-mail</a> </li>
+                    <li class="nav"><a href="index.html">Pref. de Materlândia</a></li>
+                    <li class="nav"><a href="about.html">Minas Gerais</a></li>
+                    <li class="nav"><a href="services.html">Copasa</a></li>
+                    <li class="nav"><a href="#">Cemig</a></li>
                 </ul>
             </div>
+            <div class="pt-4">
+                <ul>
+                    <li><a href="contact.html">Polícia Militar</a></li>
+                    <li><a href="#">TCU</a></li>
+                    <li><a href="contact.html">Assembleia Legislativa do Estado de Minas Gerais</a></li>
+                    <li><a href="https://webmail.hostinger.com.br/auth">Servidor de E-mail</a> </li>
+                </ul>
         </div>
     </div>
 
     <div class="container-fluid copyright text-light py-4" data-wow-delay="0.1s">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                <div class="col-md-6 text-center text-md-start mb-md-0">
                     &copy; <a href="#">© 2022 Câmara de Materlândia.</a>, All Right Reserved.
                 </div>
                 <div class="col-md-6 text-center text-md-end">
-                    Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                    <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    <br>Desenvolvido por: <a href="https://codesdev.com" target="_blank">CodesDev.com</a>
                 </div>
             </div>
         </div>

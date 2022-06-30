24661ba70ef37887c50ccfdafea3d2d8f3a33f52
Lucas Campos <bhlucascampos@gmail.com>
2022-06-30 00:39:53 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <section class="" style="clear: both;">
-  <div class="container">
-      <div class="row">
+    <div class="container">
+        <div class="row">
             <div class="footer-list-29 footer-3">     
                 <h6 class="footer-title-29">Câmara no Mapa </h6>  
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
@@ -8,17 +8,18 @@
             </div>
             
             <div>
-            <h6 class="">Entre em Contato</h6>
                 <ul>
+                    <h6 class="">Entre em Contato</h6>
                     <li><p><span class=""></span><a href="https://goo.gl/maps/m2HFq5F2Vnd4jgLu9">Pça. Geraldo Anadir, 11, Centro, Materlândia - MG 39755-000.</a></p></li>
                     <li><a href="tel:+55 33 3427-1170"><span class="fa fa-phone"></span> +55 33 3427-1170</a></li>
                     <li><a href="mailto:contato@materlandia.mg.leg.br" class="mail"><span class="fa fa-envelope-open-o"></span> contato@materlandia.mg.leg.br</a></li>
                 </ul>
             </div>
-
+        </div>
+    </div>
             <div class="">
                 <ul>
-                    <h6 class="footer-title-29">Links Úteis</h6>
+                    <h6 class="">Links Úteis</h6>
                     <li><a href="index.html">Pref. de Materlândia</a></li>
                     <li><a href="about.html">Minas Gerais</a></li>
                     <li><a href="services.html">Copasa</a></li>

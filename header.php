1cc52e4997d3a4d465ef67dd34cb5f6f5df9e680
Lucas Campos <bhlucascampos@gmail.com>
2022-06-30 00:21:05 -0300

    .

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="menu text-center collapse navbar-collapse" id="nav-target" >
-        <ul class="navbar-nav h4 ml-auto">
+        <ul class="navbar-nav h4 ml-auto ">
           <li class="nav-item mr-1">
             <a class="nav-link" href="index.php">Início</a>
           </li>

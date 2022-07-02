4baa1e5c9ef63cc28ac8fc87c43f029c922bd26c
Lucas Campos <bhlucascampos@gmail.com>
2022-07-02 16:40:44 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="ml-2 col-md-5 text-md-start mb-md-0">     
                 <h6 class="">Câmara no Mapa </h6>  
-                <iframe class="ml-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="400" height="175" frameborder="0" aria-hidden="false">
+                <iframe class="ml-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1539.8047847575906!2d-43.06082514205571!3d-18.473494252861574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xae5c5534e4c117%3A0x7f8350fc0f014ab8!2sC%C3%A2mara%20Municipal%20de%20Materl%C3%A2ndia!5e1!3m2!1spt-BR!2sbr!4v1607005357464!5m2!1spt-BR!2sbr" width="360" height="190" frameborder="0" aria-hidden="false">
                 </iframe>
             </div>
             
@@ -11,7 +11,7 @@
                 <ul>
                     <h6 class="h2">Entre em Contato</h6>
                     <li><p><span class=""></span><a href="https://goo.gl/maps/m2HFq5F2Vnd4jgLu9">Pça. Geraldo Anadir, 11, Centro, Materlândia - MG 39755-000.</a></p></li>
-                    <li><a href="tel:+55 33 3427-1170"><span class="fa fa-phone"></span> +55 33 3427-1170</a></li>
+                    <li><a href="tel:+55 33 3427-1170"><span class=""></span> +55 33 3427-1170</a></li>
                     <li><a href="mailto:contato@materlandia.mg.leg.br" class="mail"><span class=""></span> contato@materlandia.mg.leg.br</a></li>
                 </ul>
             </div>

c0a5bb60de5c71ae76c5a6922e8846d9cfc7c05e
Lucas Campos <bhlucascampos@gmail.com>
2022-07-22 08:34:48 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -46,15 +46,6 @@
                 </ul>
         </div>
     </div>
-    <!-- move top -->
-    <a id="backToTopButton" href="#home" class="">
-        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="20" cy="20" r="20" fill="#00856F" />
-            <path d="M20 27V13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M13 20L20 13L27 20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-    </a>
-    <!-- /move top -->
 </section>
 <section id="copyright">
     <div class="copyright text-light" data-wow-delay="0.1s">
@@ -69,6 +60,15 @@
             </div>
         </div>
     </div>
+    <!-- move top -->
+    <a id="backToTopButton" href="#home" class="">
+        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="20" cy="20" r="20" fill="#00856F" />
+            <path d="M20 27V13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M13 20L20 13L27 20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </a>
+    <!-- /move top -->
     
 </section>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->

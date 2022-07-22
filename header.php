b7415af1a5d8a7d84e45aa8f0b3c632a3914891b
Lucas Campos <bhlucascampos@gmail.com>
2022-07-21 21:02:57 -0300

    Update header.php

--- header.php
+++ header.php
@@ -16,16 +16,6 @@
 
 <body>
 <header class="">
-  <div class="align-items-center" style="background: black; text-align: center">
-    <div class="container">
-      <div class="">
-        <a  href="index.php" class="text-white pr-3">Câmara Municipal de Materlândia - MG</a>
-        <a href="tel:+55 33 3424-1170" class="text-white pr-3" style= "white-space: nowrap;">+55 33 3427-1170</a>
-        <a href="mailto:contato@materlandia.mg.leg.br" class="text-white pr-3"> contato@materlandia.mg.leg.br</a>
-        </div>
-      </div>
-    </div>
-    
   <nav class="barra_menu navbar navbar-expand-sm navbar-light sticky-top shadow">
     <div class="container">
       <button class="btn pull-right navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-target">
@@ -33,19 +23,19 @@
       </button>
       <div class="menu text-center collapse navbar-collapse" id="nav-target" >
         <ul class="navbar-nav h6 ml-auto ">
-          <li class="nav-item mr-1">
+          <li class="nav-item mr-2">
             <a class="nav-link" href="index.php">Início</a>
           </li>
-          <li class="nav-item mr-1">
+          <li class="nav-item mr-2">
             <a class="nav-link" href="index.php">A cidade</a>
           </li>
-          <li class="nav-item mr-1">
+          <li class="nav-item mr-2">
             <a class="nav-link" href="vereadores.php">Vereadores</a>
           </li>
-           <li class="nav-item mr-1">
+           <li class="nav-item mr-2">
             <a class="nav-link" href="regimento.php">Regimento</a>
           </li>
-          <li class="nav-item mr-1">
+          <li class="nav-item mr-2">
             <a class="nav-link" href="contato.php">Contato</a>
           </li>
         </ul>        
